196b2377f9c7e67309bda85ccdc402794f54dd93
alex136q <alexandru136@gmail.com>
2026-03-24 05:04:04 +0200

    Increased buffer sizes for debug messages.

--- debug.c
+++ debug.c
@@ -6,51 +6,52 @@ struct debug_config debug_cfg;
 
 const int bytes_per_mem_dump_line = 0x10;
 
+const int debug_buffer_size = 8192;
+
 
 void debug_f4(const char *template,
 	      const long int value0, const long int value1,
 	      const long int value2, const long int value3) {
   if(!debug_cfg.enable_debug_msgs) return;
-  char buf[1024];
-  int count = snprintf(buf, 1000, template,
-		       value0, value1, value2, value3);
-  buf[count] = 0;
+  char buf[debug_buffer_size];
+  memset(buf, 0, debug_buffer_size);
+  snprintf(buf, debug_buffer_size - 1, template, value0, value1, value2, value3);
   debug_f0(buf);
 }
 
 void debug_f3(const char *template,
 	      const long int value0, const long int value1, const long int value2) {
   if(!debug_cfg.enable_debug_msgs) return;
-  char buf[1024];
-  int count = snprintf(buf, 1000, template, value0, value1, value2);
-  buf[count] = 0;
+  char buf[debug_buffer_size];
+  memset(buf, 0, debug_buffer_size);
+  snprintf(buf, debug_buffer_size - 1, template, value0, value1, value2);
   debug_f0(buf);
 }
 
 void debug_f2(const char *template,
 	      const long int value0, const long int value1) {
   if(!debug_cfg.enable_debug_msgs) return;
-  char buf[1024];
-  int count = snprintf(buf, 1000, template, value0, value1);
-  buf[count] = 0;
+  char buf[debug_buffer_size];
+  memset(buf, 0, debug_buffer_size);
+  snprintf(buf, debug_buffer_size - 1, template, value0, value1);
   debug_f0(buf);
 }
 
 void debug_f1(const char *template,
 	      const long int value) {
   if(!debug_cfg.enable_debug_msgs) return;
-  char buf[1024];
-  int count = snprintf(buf, 1000, template, value);
-  buf[count] = 0;
+  char buf[debug_buffer_size];
+  memset(buf, 0, debug_buffer_size);
+  snprintf(buf, debug_buffer_size - 1, template, value);
   debug_f0(buf);
 }
 
 void debug_s1(const char *template,
 	      const char *value) {
   if(!debug_cfg.enable_debug_msgs) return;
-  char buf[1024];
-  int count = snprintf(buf, 1000, template, value);
-  buf[count] = 0;
+  char buf[debug_buffer_size];
+  memset(buf, 0, debug_buffer_size);
+  snprintf(buf, debug_buffer_size - 1, template, value);
   debug_f0(buf);
 }
 
@@ -58,9 +59,9 @@ void debug_f0(const char *template) {
   if(!debug_cfg.enable_debug_msgs) return;
   struct video_msg debug_msg;
   debug_msg.oper = VIDEO_CMD_WRITE;
-  debug_msg.dptr = malloc(1024);
-  int count = snprintf((char *)debug_msg.dptr, 1000, template);
-  ((char *)debug_msg.dptr)[count] = 0;
+  debug_msg.dptr = malloc(debug_buffer_size);
+  memset(debug_msg.dptr, 0, debug_buffer_size);
+  snprintf((char *)debug_msg.dptr, debug_buffer_size - 1, template);
   debug_msg.size = strlen(debug_msg.dptr);
   acquire_lock(&debug_cfg.debug_lock);
   if(debug_cfg.show_memory_dump) {
